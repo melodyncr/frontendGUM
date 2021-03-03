e29a52781030ea8c3b7753223f16bd2aaa258081
Tezaen <Philippines420AD>
2021-03-03 13:13:45 -0800

    added querying by ID to create-account-activity

--- app/src/main/java/com/example/a499_android/CreateAccount.java
+++ app/src/main/java/com/example/a499_android/CreateAccount.java
@@ -1,22 +1,37 @@
 package com.example.a499_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class CreateAccount extends AppCompatActivity {
 
-    private TextView usernameField, passwordField, emailField;
+    private TextView usernameField, passwordField;
     private Button createAccountBtn;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference users = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         wiredUp();
 
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
@@ -24,9 +39,8 @@ public class CreateAccount extends AppCompatActivity {
             public void onClick(View view) {
                 String enteredUsername = usernameField.getText().toString();
                 String enteredPassword = passwordField.getText().toString();
-                String enteredEmail = emailField.getText().toString();
                 if (checkUsernameValidity(enteredUsername) && checkPasswordValidity(
-                        enteredPassword) && checkEmailValidity(enteredEmail)) {
+                        enteredPassword)) {
                     /**
                      * Add to database. Warnings will come from the functions themselves.
                      */
@@ -42,7 +56,6 @@ public class CreateAccount extends AppCompatActivity {
     private void wiredUp() {
         usernameField = findViewById(R.id.newUsernameText);
         passwordField = findViewById(R.id.newPasswordText);
-        emailField = findViewById(R.id.newUserEmailText);
         createAccountBtn = findViewById(R.id.createAccountBtn);
     }
 
@@ -50,11 +63,44 @@ public class CreateAccount extends AppCompatActivity {
         /**
          * Create code for querying through firestore DB for enteredUsername
          */
+        final boolean[] usernameAvailable = {true};
+        String USERTAG = "User documents";
+        Log.d("Entered Username: ", enteredUsername);
+
+        DocumentReference userDocRef = users.document(enteredUsername);
+
+        userDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(USERTAG, "DocumentSnapshot data: " + document.getData());
+                        Log.d("Warning", "Uh oh username is taken");
+                        Toast.makeText(CreateAccount.this,
+                                "This username is taken! Please enter a different username",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                        usernameAvailable[0] = false;
+                    } else {
+                        Log.d(USERTAG, "No such document, This Username is available");
+                    }
+                } else {
+                    Log.d(USERTAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        /**
+         * If there was a user with entered username found already;
+         */
+
+        if(!usernameAvailable[0]) return false;
+
         if (enteredUsername.length() < 5) {
             Toast.makeText(CreateAccount.this, "This username is too short",
                     Toast.LENGTH_SHORT)
                     .show();
-
             return false;
         }
 
@@ -68,14 +114,6 @@ public class CreateAccount extends AppCompatActivity {
                     .show();
             return false;
         }
-        return true;
-    }
-
-    private boolean checkEmailValidity(String enteredEmail) {
-        /**
-         * Code to check if email is in database
-         */
-
         return true;
     }
 }
